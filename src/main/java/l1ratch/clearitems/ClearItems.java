8bc830a4d6fb2072836643e82ef2fbc94eed1423
Линк <sm171105@bk.ru>
2024-04-19 21:56:36 +0300

    Plugin Version: 3.1 The plugin code has been changed, bugs have been fixed, and a version control system has been added.

--- src/main/java/l1ratch/clearitems/ClearItems.java
+++ src/main/java/l1ratch/clearitems/ClearItems.java
@@ -2,132 +2,194 @@ package l1ratch.clearitems;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
-
-import java.io.File;
-import java.io.FileWriter;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.InputStreamReader;
+import org.bukkit.entity.Player;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class ClearItems extends JavaPlugin implements Listener {
-
-    private int clearDelay;
-    private String clearMessage;
-    private int logClearInterval;
-    private boolean enableLogging;
-    private String notification5s;
-    private String notification10s;
+    private int clearInterval;
+    private boolean highlightEnabled;
+    private String notificationPrefix;
     private String notification15s;
-    private boolean highlight15sItems;
-    private String highlight15sItems_msg1;
-    private String highlight15sItems_msg2;
-
-    private File logFile;
-    private BukkitTask highlightTask;
+    private String notification10s;
+    private String notification5s;
+    private ChatColor highlightColor;
+    private final String githubRepo = "https://api.github.com/repos/l1ratch/ClearItems/releases/latest";
+    private String latestVersion;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
-        setupLogFile();
-        startAutoClearTask();
+
+        startItemClearTask();
+
+        // Асинхронно проверяем последнюю версию плагина на GitHub
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                checkLatestVersion();
+            }
+        }.runTaskAsynchronously(this);
+
+        // Асинхронно оповещаем администратора при входе
+        getServer().getScheduler().runTaskAsynchronously(this, () -> {
+            getServer().getOnlinePlayers().forEach(this::notifyAdmin);
+        });
+    }
+
+    @EventHandler
+    public void onWorldInit(WorldInitEvent event) {
+        startItemClearTask(event.getWorld());
     }
 
     private void loadConfig() {
         FileConfiguration config = getConfig();
-        clearDelay = config.getInt("clear_delay", 60); // Default clear delay: 60 seconds
-        clearMessage = config.getString("clear_message", "The dropped item has been cleared.").replaceAll("&", "§"); // Default clear message
-        logClearInterval = config.getInt("log_clear_interval", 3600); // Default log clear interval: 1 hour
-        enableLogging = config.getBoolean("enable_logging", false); // Default enable logging: false
-        notification5s = config.getString("notification_5s", "Item will be cleared in 5 seconds.").replaceAll("&", "§");
-        notification10s = config.getString("notification_10s", "Item will be cleared in 10 seconds.").replaceAll("&", "§");
-        notification15s = config.getString("notification_15s", "Item will be cleared in 15 seconds.").replaceAll("&", "§");
-        highlight15sItems = config.getBoolean("highlight_15s_items", false); // Default highlight 15s items: false
-        highlight15sItems_msg1 = config.getString("highlight_15s_Items_msg1", "Item will be cleared in 15 seconds.");
-        highlight15sItems_msg2 = config.getString("highlight_15s_Items_msg2", " will be cleared in 15 seconds.");
-    }
-
-    private void setupLogFile() {
-        logFile = new File(getDataFolder(), "log.txt");
-        if (!logFile.exists() && enableLogging) {
-            try {
-                logFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        clearInterval = config.getInt("clear_interval", 600);
+        highlightEnabled = config.getBoolean("highlight_enabled", true);
+        notificationPrefix = ChatColor.translateAlternateColorCodes('&', config.getString("notification_prefix", "&7[ItemClear]"));
+        notification15s = ChatColor.translateAlternateColorCodes('&', config.getString("notification_15s", "&eItems will be cleared in 15 seconds!"));
+        notification10s = ChatColor.translateAlternateColorCodes('&', config.getString("notification_10s", "&eItems will be cleared in 10 seconds!"));
+        notification5s = ChatColor.translateAlternateColorCodes('&', config.getString("notification_5s", "&eItems will be cleared in 5 seconds!"));
+        String highlightColorStr = config.getString("highlight_color", "RED");
+        try {
+            highlightColor = ChatColor.valueOf(highlightColorStr.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            getLogger().warning("Invalid highlight color in config. Defaulting to RED.");
+            highlightColor = ChatColor.RED;
         }
     }
 
-    private void startAutoClearTask() {
-        getServer().getScheduler().runTaskTimerAsynchronously(this, this::clearLogFile, 0, logClearInterval * 20); // Clear log file every logClearInterval seconds
-    }
-
-    private void clearLogFile() {
-        if (enableLogging) {
-            try {
-                FileWriter writer = new FileWriter(logFile);
-                writer.write(""); // Clearing the file
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void logToFile(String message) {
-        if (enableLogging) {
-            try {
-                FileWriter writer = new FileWriter(logFile, true);
-                writer.write(message + "\n");
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @EventHandler
-    public void onItemDrop(PlayerDropItemEvent event) {
-        event.getPlayer().sendMessage(notification15s);
-        if (highlight15sItems) {
-            highlightItem(event.getItemDrop());
-        }
-        getServer().getScheduler().runTaskLater(this, () -> {
-            event.getPlayer().sendMessage(notification10s);
-        }, (clearDelay - 10) * 20); // 10 seconds before clear
-        getServer().getScheduler().runTaskLater(this, () -> {
-            event.getPlayer().sendMessage(notification5s);
-        }, (clearDelay - 5) * 20); // 5 seconds before clear
-        getServer().getScheduler().runTaskLater(this, () -> {
-            event.getItemDrop().remove();
-            event.getPlayer().sendMessage(clearMessage);
-            if (enableLogging) {
-                logToFile("[ClearItems] " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " - " + event.getPlayer().getName() + " the dropped item has been cleared.");
-            }
-        }, clearDelay * 20); // Convert seconds to ticks
-    }
-
-    private void highlightItem(Item item) {
-        highlightTask = Bukkit.getScheduler().runTaskTimer(this, () -> {
-            if (item.isValid()) {
-                String displayName = item.getItemStack().getItemMeta().getDisplayName();
-                if (displayName == null || displayName.isEmpty()) {
-                    item.setCustomName(ChatColor.RED + highlight15sItems_msg1);
-                } else {
-                    item.setCustomName(ChatColor.RED + displayName + highlight15sItems_msg2);
+    private void startItemClearTask() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (World world : Bukkit.getWorlds()) {
+                    clearItems(world);
                 }
-                item.setCustomNameVisible(true);
-            } else {
-                highlightTask.cancel();
             }
-        }, 0, 10);
+        }.runTaskTimerAsynchronously(this, clearInterval * 20L, clearInterval * 20L);
+    }
+
+    private void startItemClearTask(World world) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                clearItems(world);
+            }
+        }.runTaskTimerAsynchronously(this, clearInterval * 20L, clearInterval * 20L);
+    }
+
+    private void clearItems(World world) {
+        world.getEntitiesByClass(Item.class).forEach(Item::remove);
+        int warningTime = clearInterval - 15;
+        if (warningTime <= 0) {
+            broadcastNotification(notificationPrefix + " " + notification15s);
+            return;
+        }
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                broadcastNotification(notificationPrefix + " " + notification15s);
+            }
+        }.runTaskLaterAsynchronously(this, warningTime * 20L);
+
+        warningTime -= 5;
+        if (warningTime <= 0) {
+            broadcastNotification(notificationPrefix + " " + notification10s);
+            return;
+        }
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                broadcastNotification(notificationPrefix + " " + notification10s);
+            }
+        }.runTaskLaterAsynchronously(this, warningTime * 20L);
+
+        warningTime -= 5;
+        if (warningTime <= 0) {
+            broadcastNotification(notificationPrefix + " " + notification5s);
+            return;
+        }
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                broadcastNotification(notificationPrefix + " " + notification5s);
+            }
+        }.runTaskLaterAsynchronously(this, warningTime * 20L);
+
+        if (highlightEnabled) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    highlightItems(world);
+                }
+            }.runTaskLaterAsynchronously(this, (clearInterval - 15) * 20L);
+        }
+    }
+
+    private void broadcastNotification(String message) {
+        if (!message.isEmpty()) {
+            Bukkit.broadcastMessage(message);
+        }
+    }
+
+    // Метод для проверки последней версии плагина на GitHub
+    private void checkLatestVersion() {
+        try {
+            URL url = new URL(githubRepo);
+            URLConnection connection = url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            reader.close();
+            // Парсим JSON и получаем последнюю версию
+            latestVersion = response.toString().split("\"tag_name\":\"")[1].split("\",")[0];
+
+            // Оповещаем обновление, если доступна новая версия
+            if (isNewVersionAvailable(latestVersion)) {
+            getLogger().info(notificationPrefix + ChatColor.GREEN + "A new version of the plugin is available: " + ChatColor.GOLD + latestVersion);
+            getLogger().info(notificationPrefix + ChatColor.RED + "Download: https://github.com/l1ratch/ClearItems/releases/latest");
+            }
+
+        } catch (IOException e) {
+            getLogger().warning(notificationPrefix + ChatColor.RED + "Couldn't check the latest version of the plugin: " + e.getMessage());
+        }
+    }
+
+    // Метод для оповещения администратора
+    private void notifyAdmin(Player player) {
+        if (player.isOp()) {
+            player.sendMessage(notificationPrefix + ChatColor.RED + "A new version of the plugin is available!" + ChatColor.GOLD + "Download: https://github.com/l1ratch/ClearItems/releases/latest");
+        }
+    }
+
+    // Метод для сравнения версий и определения, доступна ли новая версия
+    private boolean isNewVersionAvailable(String latestVersion) {
+        String currentVersion = getDescription().getVersion();
+        // Простое сравнение строк версий
+        return currentVersion.compareTo(latestVersion) < 0;
+    }
+
+    private void highlightItems(World world) {
+        for (Item item : world.getEntitiesByClass(Item.class)) {
+            item.setCustomNameVisible(true);
+            item.setCustomName(highlightColor + item.getItemStack().getType().toString());
+        }
     }
 }
